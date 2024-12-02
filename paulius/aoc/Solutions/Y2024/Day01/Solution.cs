cf5af2360270a70eb284fa845f8f0451cff2dab8
Paulius Morkūnas <paulius@trafi.com>
2024-12-02 13:16:19 +0200

    2024 01 - 02

---
+++ paulius/aoc/Solutions/Y2024/Day01/Solution.cs
@@ -0,0 +1,31 @@
+﻿using MoreLinq.Extensions;
+using Tools;
+using Input = System.Collections.Generic.IEnumerable<System.Collections.Generic.IEnumerable<long>>;
+
+namespace Days.Y2024.Day01;
+
+file class Solution : ISolution<Input>
+{
+    public async Task<Input> LoadInput()
+    {
+        var input = from l in (await File.ReadAllLinesAsync(this.GetInputFile("input")))
+                let s = l.Split(' ')
+                select new[] { Convert.ToInt64(s.First()), Convert.ToInt64(s.Last()) };
+
+        return input.Transpose();
+    }
+
+    public object Part1(Input input)
+    {
+        return input.First().Order().Zip(input.Last().Order(), (a, b) => Math.Abs(a - b)).Sum();
+    }
+
+    public object Part2(Input input)
+    {
+        return (from f in Group(input.First())
+                join l in Group(input.Last()) on f.Key equals l.Key
+                select f.Key * f.Count * l.Count).Sum();
+
+        static IEnumerable<(long Key, int Count)> Group(IEnumerable<long> e) => e.GroupBy(f => f).Select(f => (f.Key, f.Count()));
+    }
+}
---
+++ paulius/aoc/Solutions/Y2024/Day02/Solution.cs
@@ -0,0 +1,33 @@
+﻿using Tools;
+using Input = System.Collections.Generic.IEnumerable<long[]>;
+
+namespace Days.Y2024.Day02;
+
+file class Solution : ISolution<Input>
+{
+    public async Task<Input> LoadInput()
+    {
+        return (from l in await File.ReadAllLinesAsync(this.GetInputFile("input"))
+               select l.Split(' ').SelectArray(long.Parse));
+    }
+
+    public object Part1(Input input)
+    {
+        return CountSafe(input).Count();
+    }
+
+    private static IEnumerable<int> CountSafe(Input input)
+    {
+        return from l in input
+                let safe = l.Zip(l.Skip(1), (a, b) => a - b).ToArray()
+                where safe.All(s => Math.Abs(s) is >= 1 and <= 3) && safe.All(s => Math.Sign(s) == Math.Sign(safe[0]))
+                select 1;
+    }
+
+    public object Part2(Input input)
+    {
+        return (from l in input
+                 let safeRemovedOne = Enumerable.Range(0, l.Length).Select(i => l.Where((n, ii) => ii != i).ToArray())
+                 select CountSafe(safeRemovedOne).Any() ? 1 : 0).Sum();
+    }
+}
