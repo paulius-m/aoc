ba074642278b4e86987fb27ee0778b731eeb24b4
Paulius Morkūnas <paulius@trafi.com>
2025-05-09 09:34:21 +0300

    add day template.

---
+++ paulius/template/Solution.cs
@@ -0,0 +1,22 @@
+﻿using Tools;
+using Input = System.Object;
+
+namespace $rootnamespace$.$fileinputname$;
+
+file class Solution : ISolution<Input>
+{
+    public async Task<Input> LoadInput()
+    {
+        return await File.ReadAllLinesAsync(this.GetInputFile("test"));
+    }
+
+    public object Part1(Input i)
+    {
+        return 0;
+    }
+
+    public object Part2(Input i)
+    {
+        return 0;
+    }
+}
