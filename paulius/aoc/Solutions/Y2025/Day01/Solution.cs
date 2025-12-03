5437504a1aa6bbb7194cdc6c4fbcb303d0e76ce4
Paulius Morkūnas <paulius@trafi.com>
2025-12-03 08:39:32 +0200

    Day01, no linq from the start, wtf.

---
+++ paulius/aoc/Solutions/Y2025/Day01/Solution.cs
@@ -0,0 +1,58 @@
+﻿using MoreLinq.Extensions;
+using Tools;
+using Input = System.Collections.Generic.IEnumerable<long>;
+
+namespace Days.Y2025.Day01;
+
+file class Solution : ISolution<Input>
+{
+    public async Task<Input> LoadInput()
+    {
+        var input = from l in await File.ReadAllLinesAsync(this.GetInputFile("input"))
+                    select l switch
+                    {
+                        ['L', .. var num] => -long.Parse(num),
+                        ['R', .. var num] => long.Parse(num),
+                    };
+
+        return input.ToArray();
+    }
+
+    public object Part1(Input input)
+    {
+        var a = 50L;
+        var zeroCount = 0L;
+        foreach (var b in input)
+        {
+            a = (a + b + 100) % 100;
+            if (a is 0)
+            {
+                zeroCount++;
+            }
+        }
+        return zeroCount;
+    }
+
+    public object Part2(Input input)
+    {
+        var a = 50L;
+        var zeroCount = 0L;
+        foreach (var b in input)
+        {
+            var newA = a + b % 100;
+
+            zeroCount += newA switch
+            {
+                > 99 => 1,
+                <= 0 when a > 0 => 1,
+                _ => 0
+            };
+
+            zeroCount += Math.Abs(b / 100);
+
+            a = (newA + 100) % 100;
+            
+        }
+        return zeroCount;
+    }
+}
